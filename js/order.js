a9bde93d1c9dad726fd1ac2f2fe16dbb15320043
simyascodehub <simyascodehub@users.noreply.github.com>
2020-06-04 23:12:25 +0530

    Orderjs

--- js/order.js
+++ js/order.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         var imgsrc = $(this).prev('a').attr('href');
         var res = encodeURIComponent(imgsrc);
         console.log(res);
-        var link = "https://api.whatsapp.com/send?phone=918921212317&text=Hello!%20I%20would%20like%20to%20order%20this%20product%20https://simyascodehub.github.io/H4U/" + res;
+        var link = "https://api.whatsapp.com/send?phone=918921212317&text=Hello!%20I%20would%20like%20to%20order%20this%20product%20https://handpicked4you.in/" + res;
         window.location = link;
     })
 });
