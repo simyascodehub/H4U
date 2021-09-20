3031b2c79e4de3941a7fcc09b1ac19cf1316e088
simyascodehub <simyascodehub@users.noreply.github.com>
2021-09-20 13:51:27 +0530

    home

--- js/main.js
+++ js/main.js
@@ -141,3 +141,15 @@ $(document).ready(function() {
 
 
 });
+// slider menu
+$( document ).ready(function() {
+    $('.box-carousel').slick({
+     dots: false,
+     arrows: true,
+     slidesToShow: 5,
+     slidesToScroll: 1,
+     prevArrow: "<button type='button' class='mission-prev-arrow'></button>",
+     nextArrow: "<button type='button' class='mission-next-arrow'></button>"
+ });
+
+});
